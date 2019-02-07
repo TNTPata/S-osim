463ab58cd349fb7fd399fa25a1fffb77c6847d53
TNTPata <putte.olsson@icloud.com>
2019-02-07 12:18:45 +0100

    Renamed a thing

--- Säosim/Säosim/Game1.cs
+++ Säosim/Säosim/Game1.cs
@@ -7,12 +7,12 @@ namespace Säosim
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class Säosim : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
-        public Game1()
+        public Säosim()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
