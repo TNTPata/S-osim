f99e9bbd1d6b6dee82ca33bd3292d8b6f5568c6b
TNTPata <putte.olsson@icloud.com>
2019-02-14 22:51:57 +0100

    Added Signals.cs. Added base class and 5 subclasses

---
+++ Säosim/Säosim/Signals.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Säosim {
+	class Signal {
+		int signalState = 0;
+		//0 = Stop, 1 = Clear
+
+
+	}
+
+	class Entrysignal {
+
+	}
+
+	class Exitsignal {
+
+	}
+
+	class Distsignal {
+
+	}
+
+	class Roadsignal {
+
+	}
+
+	class Distroadsignal {
+
+	}
+}
