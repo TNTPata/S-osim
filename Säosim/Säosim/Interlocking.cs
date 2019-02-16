f9adf84802dda5c1a008b4fce956ee4be89fdb17
TNTPata <putte.olsson@icloud.com>
2019-02-16 18:57:24 +0100

    Made all objects static to make things work. Separated object creation and object initiation.

--- Säosim/Säosim/Interlocking.cs
+++ Säosim/Säosim/Interlocking.cs
@@ -7,84 +7,144 @@ using System.Threading.Tasks;
 namespace Säosim {
 	public class Interlocking {
 
+		#region objectCreation
+		//Not sure if everything should be static, but it works that way
+		//Create the 6 switches and 2 derails
+		static Switch switch1;
+		static Switch switch2;
+		static Switch switch3;
+		static Switch switch4;
+		static Switch switch5;
+		static Switch switch6;
+
+		static Derail derail1;
+		static Derail derail2;
+
+		//Create 3 entry signals
+		static EntrySignal A;
+		static EntrySignal B;
+		static EntrySignal C;
+
+		//Create 3 exit signals
+		static ExitSignal D;
+		static ExitSignal E;
+		static ExitSignal F;
+
+		//Create distant signal
+		static DistSignal AFsi;
+
+		//Create road signal
+		static RoadSignal V1;
+
+		//Create road distant signal
+		static DistroadSignal V1Fsi;
+		#endregion
+		#region routeCreation
+		//Create arrival routes
+		static Route route_a1;
+		static Route route_a2;
+		static Route route_a3;
+		static Route route_b1;
+		static Route route_b2;
+		static Route route_b3;
+		static Route route_c1;
+		static Route route_c2;
+
+		//Create short arrival routes
+		static Route route_a2k;
+		static Route route_a3k;
+		static Route route_c1k;
+		static Route route_c2k;
+
+		//Create departure routes
+		static Route route_d1;
+		static Route route_d2;
+		static Route route_d3;
+		static Route route_e1;
+		static Route route_e2;
+		static Route route_e3;
+		static Route route_f1;
+		static Route route_f2;
+
+		//Create unmonitored route
+		static Route route_o1;
+		#endregion
+
 		//Constructor
 		public Interlocking() {
 			#region objectInit
-			//Create the 6 switches and 2 derails
-			Switch switch1 = new Switch();
-			Switch switch2 = new Switch();
-			Switch switch3 = new Switch();
-			Switch switch4 = new Switch();
-			Switch switch5 = new Switch();
-			Switch switch6 = new Switch();
+			//Init 6 switches and 2 derails
+			switch1 = new Switch();
+			switch2 = new Switch();
+			switch3 = new Switch();
+			switch4 = new Switch();
+			switch5 = new Switch();
+			switch6 = new Switch();
 
-			Derail derail1 = new Derail();
-			Derail derail2 = new Derail();
+			derail1 = new Derail();
+			derail2 = new Derail();
 
-			//Create 3 exit signals
-			ExitSignal D = new ExitSignal();
-			ExitSignal E = new ExitSignal();
-			ExitSignal F = new ExitSignal();
+			//Init 3 exit signals
+			D = new ExitSignal();
+			E = new ExitSignal();
+			F = new ExitSignal();
 
-			//Create 3 entry signals
-			EntrySignal A = new EntrySignal(E);
-			EntrySignal B = new EntrySignal(D);
-			EntrySignal C = new EntrySignal();
+			//Init 3 entry signals
+			A = new EntrySignal(E);
+			B = new EntrySignal(D);
+			C = new EntrySignal();
 
-			//Create distant signal
-			DistSignal AFsi = new DistSignal(A);
+			//Init distant signal
+			AFsi = new DistSignal(A);
 
-			//Create road signal
-			RoadSignal V1 = new RoadSignal();
+			//Init road signal
+			V1 = new RoadSignal();
 
-			//Create road distant signal(s. There are actually two, but since they display the same aspect at all times, it is counted as one for simplicity)
-			DistroadSignal V1Fsi = new DistroadSignal();
-
+			//Init road distant signal(s. There are actually two, but since they display the same aspect at all times, it is counted as one for simplicity)
+			V1Fsi = new DistroadSignal(V1);
 			#endregion
 			#region routeInit
-			//Create routes
-			//Create arrival routes
-			Route route_a1 = new Route(AFsi, A, switch1, switch6, D, B);
-			Route route_a2 = new Route(AFsi, A, switch1, switch6, switch5, switch4, D, B, C, F, derail2);
-			Route route_a3 = new Route(AFsi, A, switch1, switch6, switch5, switch4, D, B, C, F, derail2);
-			Route route_b1 = new Route(B, switch2, E, derail2, V1Fsi, V1);
-			Route route_b2 = new Route(B, switch2, switch4, switch5, E, C, F, derail2, V1Fsi, V1);
-			Route route_b3 = new Route(B, switch2, switch4, switch5, E, C, F, derail2, V1Fsi, V1);
-			Route route_c1 = new Route(C, switch6, switch5, switch1, switch4, B, F, derail2);
-			Route route_c2 = new Route(C, switch6, switch5, switch1, switch4, B, F, derail2);
+			//Init arrival routes
+			route_a1 = new Route(AFsi, A, switch1, switch6, D, B);
+			route_a2 = new Route(AFsi, A, switch1, switch6, switch5, switch4, D, B, C, F, derail2);
+			route_a3 = new Route(AFsi, A, switch1, switch6, switch5, switch4, D, B, C, F, derail2);
+			route_b1 = new Route(B, switch2, E, derail2, V1Fsi, V1);
+			route_b2 = new Route(B, switch2, switch4, switch5, E, C, F, derail2, V1Fsi, V1);
+			route_b3 = new Route(B, switch2, switch4, switch5, E, C, F, derail2, V1Fsi, V1);
+			route_c1 = new Route(C, switch6, switch5, switch1, switch4, B, F, derail2);
+			route_c2 = new Route(C, switch6, switch5, switch1, switch4, B, F, derail2);
 
-			//Create short arrival routes
-			Route route_a2k = new Route(AFsi, A, switch1, switch6, switch5, D, C, F, derail2);
-			Route route_a3k = new Route(AFsi, A, switch1, switch6, switch5, D, C, F, derail2);
-			Route route_c1k = new Route(C, switch6, switch5, switch6, F, derail2);
-			Route route_c2k = new Route(C, switch6, switch5, switch6, F, derail2);
+			//Init short arrival routes
+			route_a2k = new Route(AFsi, A, switch1, switch6, switch5, D, C, F, derail2);
+			route_a3k = new Route(AFsi, A, switch1, switch6, switch5, D, C, F, derail2);
+			route_c1k = new Route(C, switch6, switch5, switch6, F, derail2);
+			route_c2k = new Route(C, switch6, switch5, switch6, F, derail2);
 
-			//Create departure routes
-			Route route_d1 = new Route(D, switch1, switch6, A);
-			Route route_d2 = new Route(D, switch5, switch6, switch1, A, C, F);
-			Route route_d3 = new Route(D, switch5, switch6, switch1, A, C, F);
-			Route route_e1 = new Route(E, switch2, B, derail2, V1Fsi, V1);
-			Route route_e2 = new Route(E, switch4, switch2, B, derail2, V1Fsi, V1);
-			Route route_e3 = new Route(E, switch4, switch2, B, derail2, V1Fsi, V1);
-			Route route_f1 = new Route(F, switch5, switch6, switch1, C);
-			Route route_f2 = new Route(F, switch5, switch6, switch1, C);
+			//Init departure routes
+			route_d1 = new Route(D, switch1, switch6, A);
+			route_d2 = new Route(D, switch5, switch6, switch1, A, C, F);
+			route_d3 = new Route(D, switch5, switch6, switch1, A, C, F);
+			route_e1 = new Route(E, switch2, B, derail2, V1Fsi, V1);
+			route_e2 = new Route(E, switch4, switch2, B, derail2, V1Fsi, V1);
+			route_e3 = new Route(E, switch4, switch2, B, derail2, V1Fsi, V1);
+			route_f1 = new Route(F, switch5, switch6, switch1, C);
+			route_f2 = new Route(F, switch5, switch6, switch1, C);
 
-			//Create unmonitored route
-			Route route_o1 = new Route(A, B, D, E, switch1, switch2, switch6, C, F, derail2, V1Fsi, V1);
+			//Init unmonitored route
+			route_o1 = new Route(A, B, D, E, switch1, switch2, switch6, C, F, derail2, V1Fsi, V1);
 			#endregion
-
 		}
 
 		class TrackCircuit {
 			int occupationState = 0;
-			//0 = Unoccupied, 1 = Reserved, 2 = Occupied
+			//0 = Unoccupied, 1 = Occupied, 2 = Reserved
 		}
 
 		class Roadcrossing {
 
 		}
 
-		class Route { 
+		class Route {
 			#region Constructors
 			//Overload for a1. 1 distant signal, 1 entry signal, 1 switch, 1 protected switch, 2 protected signals
 			public Route(Signal includedDistSignal, Signal includedEntrySignal, Switch includeSwitch, Switch protectedSwitch, Signal protectedSignal1, Signal protectedSignal2) {
@@ -216,14 +276,18 @@ namespace Säosim {
 
 			#endregion
 
-			bool routeLocked = false;
-			List<Signal> includedSignals = new List<Signal>(); //Fill with the signals a train will pass in a specific route
-			List<Switch> includedSwitches = new List<Switch>(); //Fill with the switches and signals a train will pass in a specific route
-			List<Signal> protectedSignals = new List<Signal>(); //Fill with the signals that need to be protected/monitored in a specific route, but will not be passed by a train
-			List<Switch> protectedSwitches = new List<Switch>(); //Fill with the switches that need to be locked but are not passed by a train
-			List<Derail> protectedDerails = new List<Derail>(); //Fill with the derails that need to be locked in a specific route
+			public bool isLocked = false;
+			public List<Signal> includedSignals = new List<Signal>(); //Fill with the signals a train will pass in a specific route
+			public List<Switch> includedSwitches = new List<Switch>(); //Fill with the switches and signals a train will pass in a specific route
+			public List<Signal> protectedSignals = new List<Signal>(); //Fill with the signals that need to be protected/monitored in a specific route, but will not be passed by a train
+			public List<Switch> protectedSwitches = new List<Switch>(); //Fill with the switches that need to be locked but are not passed by a train
+			public List<Derail> protectedDerails = new List<Derail>(); //Fill with the derails that need to be locked in a specific route
 		}
-		
-		
+
+		public class Routesetter {
+			void Test() {
+
+			}
+		}
 	}
 }
