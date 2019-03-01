dab29f04faa1f999219fb6280562480d1a2a1428
TNTPata <putte.olsson@icloud.com>
2019-03-01 21:54:50 +0100

    Added Component class

---
+++ Säosim/Säosim/Component.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Säosim {
+	public abstract class Component {
+		public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+		public abstract void Update(GameTime gametime);
+	}
+}
