27603e1ee66f669d4db3cab799ca0db30c2fd9ae
TNTPata <putte.olsson@icloud.com>
2019-03-11 21:04:47 +0100

    Cleaned up/imported/copied button class

--- Säosim/Säosim/Button.cs
+++ Säosim/Säosim/Button.cs
@@ -13,100 +13,68 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Säosim {
-	/*class Button {
-		/// <summary>
-		/// Don't touch anything
-		/// </summary>
-		public enum State {
-			None,
-			Pressed,
-			Hover,
-			Released
-		}
-
-		private Rectangle _rectangle;
-
-		public State state { get; set; }
-
-		private Dictionary<State, Texture2D> _textures;
-
-		public Button(Rectangle rectangle, Texture2D noneTexture, Texture2D hoverTexture, Texture2D pressedTexture) {
-			_rectangle = rectangle;
-			_textures = new Dictionary<State, Texture2D>
-				{
-				{ State.None, noneTexture },
-				{ State.Hover, hoverTexture },
-				{ State.Pressed, pressedTexture }
-				};
-		}
-
-		public void Update(MouseState mouseState) {
-			if (_rectangle.Contains(mouseState.X, mouseState.Y)) {
-				if (mouseState.LeftButton == ButtonState.Pressed) {
-					state = State.Pressed;
-				}
-				else {
-					state = state == State.Pressed ? State.Released : State.Hover;
-				}
-			}
-			else {
-				state = State.None;
-			}
-		}
-
-		// Make sure Begin is called on s before you call this function
-		public void Draw(SpriteBatch s) {
-			s.Draw(_textures[state], _rectangle);
-		}
-	}*/
-
-	enum State {
-		None, 
-		Pressed,
-		Released
-	}
-	//I have no idea what any of this does...
-	class Button : Component {
-
+	public class Button : Component {
 		#region Fields
+
 		private MouseState _currentMouse;
+
 		private SpriteFont _font;
+
 		private bool _isHovering;
+
 		private MouseState _previousMouse;
-		private Texture2D _texturePressed;
-		private Texture2D _textureReleased;
+
+		private Texture2D _texture;
+
 		#endregion
+
 		#region Properties
+
 		public event EventHandler Click;
 
 		public bool Clicked { get; private set; }
 
-		public Color PenColor { get; set; }
+		public Color PenColour { get; set; }
 
 		public Vector2 Position { get; set; }
 
-		public Rectangle rectangle {
+		public Rectangle Rectangle {
 			get {
-				return new Rectangle((int)Position.X, (int)Position.Y, _texturePressed.Width, _texturePressed.Height);
+				return new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
 			}
 		}
 
 		public string Text { get; set; }
 
 		#endregion
+
 		#region Methods
 
-		//Constuctors
-		public Button(Texture2D texturePressed, Texture2D textureReleased) {
-			_texturePressed = texturePressed;
-			_textureReleased = textureReleased;
+		public Button(Texture2D texture, SpriteFont font) {
+			_texture = texture;
+
+			_font = font;
+
+			PenColour = Color.Black;
 		}
 
 		public override void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
-			
+			var colour = Color.White;
+
+			if (_isHovering)
+				colour = Color.Gray;
+
+			spriteBatch.Draw(_texture, Rectangle, colour);
+
+			if (!string.IsNullOrEmpty(Text)) {
+				var x = (Rectangle.X + (Rectangle.Width / 2)) - (_font.MeasureString(Text).X / 2);
+				var y = (Rectangle.Y + (Rectangle.Height / 2)) - (_font.MeasureString(Text).Y / 2);
+
+				spriteBatch.DrawString(_font, Text, new Vector2(x, y), PenColour);
+			}
 		}
 
-		public override void Update(GameTime gametime) {
+		public override void Update(GameTime gameTime) {
 			_previousMouse = _currentMouse;
 			_currentMouse = Mouse.GetState();
 
@@ -114,15 +82,15 @@ namespace Säosim {
 
 			_isHovering = false;
 
-			if (mouseRectangle.Intersects(rectangle)) {
+			if (mouseRectangle.Intersects(Rectangle)) {
 				_isHovering = true;
 
 				if (_currentMouse.LeftButton == ButtonState.Released && _previousMouse.LeftButton == ButtonState.Pressed) {
 					Click?.Invoke(this, new EventArgs());
-
 				}
 			}
 		}
+
 		#endregion
 	}
 }
